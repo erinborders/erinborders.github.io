80a635b3afe21d6b69ceabaf8f77ab70bb51b003
Erin Borders <borders16@gmail.com>
2019-05-31 19:18:46 -0400

    added javascript and smooth scrolling features. NOW I'M DONE.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// $("button").click(function() {
+//     alert("Clicked!");
+//   });
+
+var button = document.querySelector("#submit");
+
+button.addEventListener("click", function(){ alert("Thanks!"); });
