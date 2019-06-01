7cbc2c0fa04c43a2bf533c0748e1c318360f04cc
Erin Borders <borders16@gmail.com>
2019-05-31 20:25:25 -0400

    made the social media links actually go to my social medias. okay NOW i'm done.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-// $("button").click(function() {
-//     alert("Clicked!");
-//   });
-
 var button = document.querySelector("#submit");
 
 button.addEventListener("click", function(){ alert("Thanks!"); });
